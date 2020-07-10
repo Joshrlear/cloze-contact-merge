b0ffcd3b3932573a1e247ba9e57ee588da1ed549
Joshrlear <joshrlear@gmail.com>
2020-07-10 09:19:33 -0700

    working

--- scrub.js
+++ scrub.js
@@ -12,11 +12,12 @@ let i = -1
 // i            = last row pushed to scrubbed
 // j            = current column
 
-function handleInteractionMerges(i, j, curr, prev, cell) {
-    const currCell = curr[j]
-    // i = last row pushed to scrubbed
-    // j = current column
 
+
+
+
+function handleInteractionMerges(i, j, curr) {
+    const currCell = curr[j]
     const firstInteraction = headerRow[j].includes("First Interaction")
     const lastInteraction = headerRow[j].includes("Last Interaction")
     const interactionScore = headerRow[j].includes("Score")
@@ -24,7 +25,6 @@ function handleInteractionMerges(i, j, curr, prev, cell) {
 
     // if none present, skip.
     if (!firstInteraction && !lastInteraction && !interactionScore) {
-        console.log("Nope-------:", i, currIndex, headerRow[j], "currCell:", currCell)
         return false
     }
 
@@ -32,20 +32,13 @@ function handleInteractionMerges(i, j, curr, prev, cell) {
     const directionOrtype = headerRow[j].includes("Direction") || headerRow[j].includes("Type") ? true : false
 
     if (firstInteraction) {
-        if (directionOrtype) { // sm, use scrubbed
-
-            if (curr[0].match("Amber Cole")) console.log("sm - ", curr[0], "Prev:", `${prev[j]} = {${i + 1}: ${j + 1}}`, "Curr:", `${cell} = {${currIndex + 1}: ${j + 1}}`)
+        if (directionOrtype) {
                                     // mainlist[detailsINeed].correctForUnwantedCharacters 
             const rowForInteractions = data[correspondingRow].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
-            if (curr[0].match("Amber Cole")) console.log("rowInteractions:", rowForInteractions[j])
             scrubbed[i][j] = rowForInteractions[j] ? rowForInteractions[j] : scrubbed[i][j] // use correct direction
-            if (curr[0].match("Amber Cole")) console.log("To:", scrubbed[i][j])
         }
-
-        // find smallest number
-        else {
+        else { // find smallest number
             sm = Math.min(scrubbed[i][j], currCell) // use smallest number
-            console.log(curr[0], "sm:", sm, scrubbed[i][j], i + 1, currIndex + 1)
             // find corresponding row
             correspondingRow = scrubbed[i][j] < currCell ? currIndex - 1 : currIndex
             // Apply correct date/score to scrubbed
@@ -54,22 +47,16 @@ function handleInteractionMerges(i, j, curr, prev, cell) {
         }
     }
     else {
-        if (directionOrtype) { // lg, use curr cell
-
-            if (curr[0].match("Amber Cole")) console.log("lg - ", curr[0], "Prev:", `${prev[j]} = {${i + 1}: ${j + 1}}`, "Curr:", `${cell} = {${currIndex + 1}: ${j + 1}}`)
+        if (directionOrtype) {
                                     // mainlist[detailsINeed].correctForUnwantedCharacters 
             const rowForInteractions = data[correspondingRow].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/)
-            if (curr[0].match("Amber Cole")) console.log("rowInteractions:", rowForInteractions[j])
             scrubbed[i][j] = rowForInteractions[j] ? rowForInteractions[j] : scrubbed[i][j] // use correct direction
-            if (curr[0].match("Amber Cole")) console.log("To:", scrubbed[i][j])
         }
         
-        // use largest number
-        else {
+        else { // use largest number
             lg = Math.max(scrubbed[i][j], currCell) 
-            console.log(curr[0], "lg:", lg, scrubbed[i][j], i + 1, currIndex + 1)
             // find corresponding row
-            correspondingRow = scrubbed[i][j] > currCell ? currIndex - 1 : currIndex  //i : currIndex
+            correspondingRow = scrubbed[i][j] > currCell ? currIndex - 1 : currIndex
             // Apply correct date/score to scrubbed
             // if sm is truthy use sm otherwiase use lg
             scrubbed[i][j] = sm ? sm : lg
@@ -79,11 +66,12 @@ function handleInteractionMerges(i, j, curr, prev, cell) {
 }
 
 
+
+
+
 function handleDupNamedRows(i, curr) {
     const prev = scrubbed[i]    // last uniquely named row
     let j = -1                   // column
-
-    //console.log("Name:", curr[0]) // name
 
     for (cell of curr) {
         ++j
@@ -92,7 +80,7 @@ function handleDupNamedRows(i, curr) {
             
             if (prev[j].length && cell.length) { // neither empty - merge
                 
-                if (handleInteractionMerges(i, j, curr, prev, cell)) continue
+                if (handleInteractionMerges(i, j, curr)) continue
 
                 // if cells are not "interaction types" continue to merge
                 scrubbed[i][j] = `"${scrubbed[i][j]}, ${cell}"`
@@ -106,6 +94,10 @@ function handleDupNamedRows(i, curr) {
     }
 }
 
+
+
+
+
 for (currRow of data) {
     
     // create array of cells
@@ -116,7 +108,6 @@ for (currRow of data) {
     !headerRow && (headerRow = currArrayOfCells)
     
     if (prevName === currName) {
-        //console.log("Row:", i + 1, currArrayOfCells[0], currArrayOfCells[77], currArrayOfCells[78])
         handleDupNamedRows(i, currArrayOfCells)
 
         // skip pushing this row b/c duplicate
@@ -125,7 +116,6 @@ for (currRow of data) {
         prevName = ''                                    
     }
     else {
-
         scrubbed.push(currArrayOfCells)                 // push only if currRow not same name
         prevArrayOfCells = currArrayOfCells             // set prev to current cells
         prevName = currName                             // set prev to current name
